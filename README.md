b74d14b1e1ddf3f12185b7bac692810709295740
Melissa Arrington <melissa.arrington1@yahoo.com>
2018-11-08 10:14:29 -0500

    added .env file and connected database via mongo uri

--- README.md
+++ README.md
@@ -6,3 +6,6 @@ Tea App
 This is a full stack application, that allows a user to create an account, and select Teas to put in their database.
 
 This app uses full CRUD methods, Node.js, Express.js, MongoDB, Mongoose, and Handlebars.
+
+
+5bd9cea3a13b3c1dd5134186
--- db/connection.js
+++ db/connection.js
@@ -2,11 +2,11 @@
 const mongoose = require('mongoose');
 
 require('dotenv').config();
-mongoose.connect('mongodb://localhost/teatree') 
-mongoose.connection.once('open', () => {
-    console.log(`Mongoose has connected to MongoDB`)
-})
-//mongoose.connect(process.env.MONGODB_URI).then(() => {
-//console.log('Connected to MongoDB')
+// mongoose.connect('mongodb://localhost/teatree') 
+// mongoose.connection.once('open', () => {
+//     console.log(`Mongoose has connected to MongoDB`)
+// })
+mongoose.connect(process.env.MONGODB_URI).then(() => {
+console.log('Connected to MongoDB')})
 
 module.exports = mongoose
